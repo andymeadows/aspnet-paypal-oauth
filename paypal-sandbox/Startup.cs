0efb34e61f057122547c42f8fa27ffab19dad845
Andy Meadows <andy.meadows@gmail.com>
2015-09-20 13:22:14 -0400

    Initial commit.

---
+++ paypal-sandbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(paypal_sandbox.Startup))]
+namespace paypal_sandbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
